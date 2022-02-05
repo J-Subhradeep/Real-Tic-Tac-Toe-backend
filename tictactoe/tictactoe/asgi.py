d9afc7c8d874f0e124c3d7a38a1715e8469ee879
Subhradeep <subhradeeppal30@gmail.com>
2022-02-05 22:47:53 +0530

    no change

--- tictactoe/tictactoe/asgi.py
+++ tictactoe/tictactoe/asgi.py
@@ -8,14 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-import django
+
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import realtime.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tictactoe.settings')
-django.setup()
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket":
